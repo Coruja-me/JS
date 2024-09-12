210ca38c248cb41125476b1e379b0d60d071ca99
Coruja <cauavalves@gmail.com>
2024-09-12 16:30:43 -0300

    init menu do app

--- Trilha Iniciante/index.js
+++ Trilha Iniciante/index.js
@@ -1,4 +1,37 @@
-function Start() {
+const {select} = require('@inquirer/prompts')
+
+async function Start() {
     console.log("Bem Vindo!")
+
+    while(true){
+        const opcao = await select({
+            message: "Menu",
+            choices: [
+                {
+                    name:"Cadastrar meta",
+                    value: "cad"
+                },
+                {
+                    name:"Listar metas",
+                    value: "list"
+                },
+                {
+                    name:"Sair",
+                    value: "exit"
+                }
+            ]
+        })
+        switch(opcao){
+            case "cad":
+                console.log("Cadastro!")
+            break
+            case "list":
+                console.log("Cadastro!")
+            break
+            case "exit":
+                console.log("Saindo!")
+            return
+        }
+    }
 }
 Start()
