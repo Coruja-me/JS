aa32e1049419c699f48b7c809191e30b32c8bc5c
Coruja <cauavalves@gmail.com>
2024-09-13 15:50:50 -0300

    git commit -m"add: Deletar(), update: Listar()"

--- Trilha Iniciante/index.js
+++ Trilha Iniciante/index.js
@@ -17,16 +17,19 @@ async function Cadastrar(){
 async function Listar() {
     const resposta = await checkbox({
         message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar",
-        choices: [...metas]
+        choices: [...metas],
+        instructions: false
+    })
+
+    metas.forEach((m) => {
+        m.checked = false
     })
 
     if(resposta.length == 0){
         console.log("Nenhuma meta selecionada!")
         return
     }
-    metas.forEach((m) => {
-        m.checked = false
-    })
+    
     resposta.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
@@ -45,10 +48,48 @@ async function MetasFeitas() {
         return
     }
     await select({
-        message: "Metas Concluídas:",
+        message: "Metas Concluídas: "+ feitas.length,
         choices: [...feitas]
     })
 }
+
+async function MetasAbertas() {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+    if(abertas.length == 0){
+        console.log("Não há metas abertas!")
+        return
+    }
+    await select({
+        message: "Metas A Serem Feitas: "+ abertas.length,
+        choices: [...abertas]
+    })
+}
+
+async function Deletar(){
+    const metanaofeita = metas.map((meta) => {
+        return {value: meta.value, checked: false}
+    })
+    
+    const deletar = await checkbox({
+        message: "Selecione a meta: ",
+        choices: [...metanaofeita],
+        instructions: false
+    })
+
+    if(deletar.length == 0){
+        console.log("Não há metas para apagar!")
+        return
+    }
+    deletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+    console.log("Meta(s) deletada(s)!")
+}
+
 
 async function Start() {
     console.log("Bem Vindo!")
@@ -66,8 +107,8 @@ async function Start() {
                     value: "list"
                 },
                 {
-                    name: "Ver metas feitas",
-                    value: "done"
+                    name: "Remover metas",
+                    value: "del"
                 },
                 {
                     name: "Sair",
@@ -80,10 +121,37 @@ async function Start() {
                 await Cadastrar()
             break
             case "list":
-                await Listar()
+                const list = await select({
+                    message: "Que tipo de listagem gostaria?",
+                    choices: [
+                        {
+                            name: "Ver todas as metas",
+                            value: "all"
+                        },
+                        {
+                            name: "Ver apenas metas abertas",
+                            value: "open"
+                        },
+                        {
+                            name: "Ver apenas metas feitas",
+                            value: "done"
+                        },
+                    ]
+                })
+                switch (list) {
+                    case "all":
+                        await Listar()
+                    break;
+                    case "done":
+                        await MetasFeitas()
+                    break
+                    case "open":
+                        await MetasAbertas()
+                    break
+                }
             break
-            case "done":
-                await MetasFeitas()
+            case "del":
+                await Deletar()
             break
             case "exit":
                 console.log("Saindo!")
